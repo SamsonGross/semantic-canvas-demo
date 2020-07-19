75e9e782d751ce3f11e8b6e9e2298cd9b84a4111
Samson Gross <32847363+SamsonGross@users.noreply.github.com>
2020-07-19 15:15:11 +0200

    added semantic-canvas-core-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SemanticCanvasCoreModule } from '@semantic-canvas/semantic-canvas-core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SemanticCanvasCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
